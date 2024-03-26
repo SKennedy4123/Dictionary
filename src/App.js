43b710722b907c6315527efaa1fedc8b188efe1a
SKennedy4123 <148784364+SKennedy4123@users.noreply.github.com>
2024-03-26 11:12:58 -0400

    added Dictionary.js and Dictionary.css, initial search function and alert with search value

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React from "react";
 import logo from './logo.png';
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HEllo
-        </p>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        This project is coded by Sarah Kennedy.
+      </footer>
+    </div>
     </div>
   );
 }
