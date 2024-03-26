cfda94e053e3fa353b5c2ce081368ae782bc2bc4
SKennedy4123 <148784364+SKennedy4123@users.noreply.github.com>
2024-03-26 13:06:57 -0400

    rendered Meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
+        setResults(response.data);
     }
 
     function search(event) {
         event.preventDefault();
-    }
-
     let apiKey = "87fd924aft47ob31f6228617ba830bdb";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+    }
 
 
     function handleKeywordChange(event) {
@@ -25,8 +27,9 @@ export default function Dictionary() {
     return (
     <div className="Dictionary">
         <form onSubmit={search}>
-            <input type="search" onChange={handleKeywordChange} />
+            <input type="search" onChange={handleKeywordChange} autoFocus={true}/>
         </form>
+    <Results results={results}/>
     </div>
     );
 }
