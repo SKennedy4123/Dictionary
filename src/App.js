4b87c40309c8ed600553e9bb67f76c7fc1c6be94
SKennedy4123 <148784364+SKennedy4123@users.noreply.github.com>
2024-03-26 14:11:55 -0400

    added GitHub and Netlify links in footer

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Dictionary />
       </main>
       <footer className="App-footer">
-        This project is coded by Sarah Kennedy.
+        This project is coded by Sarah Kennedy, open-sourced on <a href="https://github.com/SKennedy4123/Dictionary" target="_blank" rel="noreferrer">GitHub</a>, and hosted on <a href="https://dictionary-kennedy.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.
       </footer>
     </div>
     </div>
