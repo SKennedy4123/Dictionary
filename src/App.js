a8c399136b6f4ca32a17401647b22ce8eb1289c1
SKennedy4123 <148784364+SKennedy4123@users.noreply.github.com>
2024-03-27 11:24:15 -0400

    add photos and styling in css

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ function App() {
         <Dictionary />
       </main>
       <footer className="App-footer">
-        This project is coded by Sarah Kennedy, open-sourced on <a href="https://github.com/SKennedy4123/Dictionary" target="_blank" rel="noreferrer">GitHub</a>, and hosted on <a href="https://dictionary-kennedy.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.
+            <a href="https://www.pexels.com">Photos provided by Pexels</a>
+           <div>
+        This project is coded by <a href="https://www.linkedin.com/in/sarah-kennedy-02b2b6293/" target="_blank" rel="noreferrer">Sarah Kennedy</a>, open-sourced on <a href="https://github.com/SKennedy4123/Dictionary" target="_blank" rel="noreferrer">GitHub</a>, and hosted on <a href="https://dictionary-kennedy.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.
+      </div>
       </footer>
     </div>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,21 +2,30 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
         setResults(response.data);
+    }
+
+    function handleImgResponse(response) {
+        setPhotos(response.data.photos);
     }
 
     function search(event) {
         event.preventDefault();
     let apiKey = "87fd924aft47ob31f6228617ba830bdb";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
-    }
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let apiImgUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;    
+    axios.get(apiImgUrl).then(handleImgResponse);
+}
 
 
     function handleKeywordChange(event) {
@@ -34,6 +43,7 @@ export default function Dictionary() {
         </div>
         <div className="hint">Suggested words: sunset, yoga, breakfast, etc.</div>
     <Results results={results}/>
+    <Photos photos={photos}/>
     </div>
     );
 }
